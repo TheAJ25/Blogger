62a5670ea6544d9a3f4b0c66fa1592c1d2c358ef
Abhijit Shaw <abhijitshaw2501@gmail.com>
2024-09-06 03:11:17 +0530

    minor changes 1

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -5,7 +5,7 @@ import { assets } from "@/Assets/assets";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const page = () => {
+const Page = () => {
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     title: "",
@@ -108,4 +108,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -6,7 +6,7 @@ import Footer from "@/Components/Footer";
 import Link from "next/link";
 import axios from "axios";
 
-const page = ({ params }) => {
+const Page = ({ params }) => {
   const [data, setData] = useState(null);
 
   const fetchBolgData = async () => {
@@ -88,4 +88,4 @@ const page = ({ params }) => {
   );
 };
 
-export default page;
+export default Page;
